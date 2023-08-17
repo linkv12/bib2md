2787421c468d19e2d5104cf6dc972a8a5d042b6f
linkv12 <thehellsingingslicer@gmail.com>
2023-08-18 02:35:57 +0700

    feat: init bibparser

---
+++ bibparser.py
@@ -0,0 +1,5 @@
+# handle parsing bib
+# output -> list of neat dict ordered asc by id
+#           with id added
+#       {type : book, id: 1, info: {key: val}}
+#       info -> {author: bla, year: 2017, publisher: O'Reilly Media Inc., title: book_title}
